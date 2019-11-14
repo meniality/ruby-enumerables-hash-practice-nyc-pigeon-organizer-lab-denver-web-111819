24d65639bde741506f388f4ff6d005021760b8c8
patrick hoehn <patrh_99@hotmail.com>
2019-11-14 00:13:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -13,7 +13,7 @@ def nyc_pigeon_organizer(data)
           new_hash[name] = key
         end
         
-        new_hash[name][key] = new_value.to_s
+        new_hash[name][key].push(new_value.to_s)
       end
     end
   end
